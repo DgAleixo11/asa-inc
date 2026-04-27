cafc71f38b8795922a53f9e93d767dca75be9dd2
Diogo Aleixo de Andrade <133553343+DgAleixo11@users.noreply.github.com>
2026-04-27 18:10:52 +0000

    fix: downgrade prisma para v6 e ajusta client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,13 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
   prisma?: PrismaClient;
 };
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL não está definida.");
-}
-
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaPg({ connectionString }),
     log: ["query", "warn", "error"],
   });
 
